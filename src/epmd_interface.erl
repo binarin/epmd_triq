a055bfd03bf84bbacb44f25520f94774cc1a0b5d
Alexey Lebedeff <alebedev@mirantis.com>
2015-12-11 21:10:32 +0300

    Implement first working epmd test

--- src/epmd_interface.erl
+++ src/epmd_interface.erl
@@ -1,20 +1,10 @@
--define(DD(__Fmt, __Args),
-        begin
-            __UserMsg = io_lib:format(__Fmt, __Args),
-            {{__Year, __Month, __Day}, {__Hour, __Minute, __Second}} = calendar:now_to_local_time(erlang:timestamp()),
-            __Msg = io_lib:format("[~4..0B-~2..0B-~2..0BT~2..0B:~2..0B:~2..0B ~s ~s:~B] ~s~n",
-                                  [__Year, __Month, __Day, __Hour, __Minute, __Second, node(), ?MODULE, ?LINE, __UserMsg]),
-            file:write_file("/tmp/erl-debug.log", __Msg, [append]),
-            error_logger:info_msg("~s", [__Msg])
-        end).
-
 -module(epmd_interface).
 
 -behaviour(gen_server).
 
 %% API
--export([start_link/0]).
--export([names/0, start_fake/1]).
+-export([start_link/0, stop/0]).
+-export([names/0, start_fake/1, stop_fake/1]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -31,6 +21,10 @@
 %%% API
 %%%===================================================================
 
+-spec stop() -> ok.
+stop() ->
+    gen_server:stop(?SERVER).
+
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
@@ -42,41 +36,55 @@ names() ->
 start_fake(NodeName) ->
     gen_server:call(?SERVER, {start_fake, NodeName}).
 
+-spec stop_fake(atom()) -> ok | not_running.
+stop_fake(NodeName) ->
+    gen_server:call(?SERVER, {stop_fake, NodeName}).
+
 %%%===================================================================
 %%% gen_server callbacks
 %%%===================================================================
 
 init([]) ->
-    EpmdPort = open_port({spawn_executable, "/usr/bin/epmd"},
-                         [{args, ["-port", "6666"]}, exit_status]), 
-    PseudoBinary = code:priv_dir(epmd_triq) ++ "/pseudo_node",
-    ?DD("~p", [PseudoBinary]),
-    {ok, #state{epmd_port = EpmdPort
-               ,pseudo_nodes = #{}
-               ,pseudo_binary = PseudoBinary}}.
+    {ok, #state{epmd_port = start_epmd_daemon()
+               ,pseudo_nodes = []
+               ,pseudo_binary = find_pseudo_node_binary()}}.
 
 handle_call(names, _From, State) ->
-    NamesBlob = os:cmd("epmd -port 6666 -names"),
-    Names = case re:run(NamesBlob, "^name (.+?) at port ([0-9]+)", [multiline, global, {capture, all_but_first, list}]) of
-                {match, RawNamesAndPorts} ->
-                    [list_to_atom(Name) || [Name, _Port] <- RawNamesAndPorts];
-                _ ->
-                    []
-            end, 
-    {reply, Names, State};
-handle_call({start_fake, NodeName}, _From, #state{pseudo_nodes = PseudoNodes} = State) ->
-    FakePort = open_port({spawn_executable, State#state.pseudo_binary}, 
-                         [exit_status, use_stdio, {args, ["--name", atom_to_binary(NodeName, utf8)]}]),
-    NewPseudoNodes = PseudoNodes#{NodeName => FakePort}, 
-    Reply = ok, 
-    {reply, Reply, State#state{pseudo_nodes = NewPseudoNodes}};
+    {reply, get_names_from_running_epmd(), State};
+handle_call({start_fake, NodeName}, _From, State) ->
+    StartResult = start_fake_node_process(NodeName, State#state.pseudo_binary),
+    handle_start_fake_node_result(StartResult, NodeName, State);
+handle_call({stop_fake, NodeName}, _From, State) ->
+    handle_stop_fake_call(lists:keyfind(NodeName, 1, State#state.pseudo_nodes), State);
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
 
+handle_start_fake_node_result({ok, _Port}, _NodeName, State) ->
+    {reply, ok, State};
+handle_start_fake_node_result(rejected, _NodeName, State) ->
+    {reply, ok, State}.
+
+start_fake_node_process(NodeName, ExecBinary) ->
+    Port = open_port({spawn_executable, ExecBinary},
+                     [exit_status, use_stdio, stream, binary,
+                      {args, ["--port", "6666", "--name", atom_to_binary(NodeName, utf8)]}]),
+    receive
+        {Port, {data, <<"ok">>}} ->
+            {ok, Port};
+        {Port, {data, <<"rejected">>}} ->
+            rejected;
+        {Port, {exit_status, _}} ->
+            early_exit
+    end.
+
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
+handle_info({EpmdPort, {exit_status, _}}, #state{epmd_port = EpmdPort} = State) ->
+    {stop, epmd_lost, State};
+handle_info({Port, {exit_status, ExitStatus}}, State) ->
+    {noreply, handle_port_exit_status(Port, ExitStatus, State)};
 handle_info(_Info, State) ->
     {noreply, State}.
 
@@ -89,3 +97,39 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+start_epmd_daemon() ->
+    Port = open_port({spawn_executable, find_epmd_proxy_binary()},
+                     [{args, ["-port", "6666"]}, exit_status, use_stdio]),
+    timer:sleep(50),
+    Port.
+
+find_epmd_proxy_binary() ->
+    code:priv_dir(epmd_triq) ++ "/epmd_proxy".
+
+find_pseudo_node_binary() ->
+    code:priv_dir(epmd_triq) ++ "/pseudo_node".
+
+get_names_from_running_epmd() ->
+    NamesBlob = os:cmd("epmd -port 6666 -names"),
+    Names = case re:run(NamesBlob, "^name (.+?) at port ([0-9]+)", [multiline, global, {capture, all_but_first, list}]) of
+                {match, RawNamesAndPorts} ->
+                    [list_to_atom(Name) || [Name, _Port] <- RawNamesAndPorts];
+                _ ->
+                    []
+            end,
+    Names.
+
+handle_port_exit_status(Port, _ExitStatus, #state{pseudo_nodes = Nodes} = State) ->
+    case lists:keymember(Port, 2, Nodes) of
+        true ->
+            State#state{pseudo_nodes = lists:keydelete(Port, 2, Nodes)};
+        false ->
+            State
+    end.
+
+handle_stop_fake_call(false, State) ->
+    {reply, not_running, State};
+handle_stop_fake_call({NodeName, Port}, State) ->
+    Port ! {self(), close},
+    NewNodes = lists:keydelete(NodeName, 1, State#state.pseudo_nodes),
+    {reply, ok, State#state{pseudo_nodes = NewNodes}}.
--- src/epmd_statem.erl
+++ src/epmd_statem.erl
@@ -3,3 +3,42 @@
 -include_lib("triq/include/triq_statem.hrl").
 -compile([export_all]).
 
+-define(NODES, [a, b, c, d]).
+
+-record(state, {started :: list(atom())
+               ,not_started :: list(atom())
+               }).
+
+prop_epmd_statem() ->
+    ?FORALL(Cmds,
+            commands(?MODULE),
+            begin
+                catch(epmd_interface:stop()),
+                {ok, _} = epmd_interface:start_link(),
+                run_commands(?MODULE, Cmds),
+                ok == epmd_interface:stop()
+            end).
+
+initial_state() ->
+    #state{started = [], not_started = ?NODES}.
+
+command(#state{not_started = [], started = [_|_] = StopCandidates}) ->
+    {call, epmd_interface, stop_fake, [oneof(StopCandidates)]};
+command(#state{not_started = [_|_] = StartCandidates, started = []}) ->
+    {call, epmd_interface, start_fake, [oneof(StartCandidates)]};
+command(#state{not_started = [_|_] = Candidates, started = [_|_] = StopCandidates}) ->
+    oneof([{call, epmd_interface, start_fake, [oneof(Candidates)]}
+          ,{call, epmd_interface, stop_fake, [oneof(StopCandidates)]}
+          ]).
+
+precondition(_, _) ->
+    true.
+
+postcondition(_, _, _) ->
+    true.
+
+next_state(#state{started = Started, not_started = NotStarted} = State, _Var, {call, epmd_interface, start_fake, [Node]}) ->
+    State#state{not_started = lists:delete(Node, NotStarted)
+               ,started = [Node] ++ lists:delete(Node, Started)};
+next_state(State, _, _) ->
+    State.
